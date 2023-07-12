25fe7ede813ad9f021a12c4133d260318f6eaef9
Omar <omarosorio489@gmail.com>
2023-07-12 15:08:25 -0600

    first commit

---
+++ store/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre 
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
